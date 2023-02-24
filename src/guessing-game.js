7394543424c2ebfd1f0cdb01c3368d926c531122
LqosL <camilla_agliullina@mail.ru>
2023-02-24 16:35:26 +0600

    feat: solved

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.range = [0, 10]
+    }
 
     setRange(min, max) {
+        this.range = [min, max]
 
     }
 
     guess() {
-
+        return Math.round((this.range[1]-this.range[0])/2 + this.range[0])
     }
 
     lower() {
-
+        this.range = [this.range[0], this.guess()]
     }
 
     greater() {
+        this.range = [ this.guess(), this.range[1]]
 
     }
 }
